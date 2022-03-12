9611039c4f09e1651b14c019ae4cf0092b32601b
minhnp9 <minhnp9@vingroup.net>
2022-03-12 22:22:59 +0700

    init fever_branch

---
+++ actions/action_extract_fever_level.py
@@ -0,0 +1,26 @@
+import logging
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+logger = logging.getLogger(__name__)
+
+class ActionExtractFeverLevel(Action):
+
+    def name(self) -> Text:
+        return "action_extract_fever_level"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        try:
+            last_entities = list(tracker.get_latest_entity_values('fever_level'))
+            if last_entities:
+                return [SlotSet(last_entities[0], True)]
+
+            return []
+        except Exception as ex:
+            logger.exception(ex)
+            return []
